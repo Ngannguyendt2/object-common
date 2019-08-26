162fe358b3afa3dcf12b1f215c37572dfad332bc
Ngannguyendt2 <ngannguyendt2haui@gmail.com>
2019-08-26 16:20:11 +0700

    object math

---
+++ Math.js
@@ -0,0 +1,14 @@
+function mathObject() {
+    let number1=Math.floor(Math.random()*10);
+    let number2=Math.floor(Math.random()*400);
+    let number3=Math.floor((Math.random()*100)-(Math.random()*50));
+    let min=Math.min(5,7,2,9);
+    let exp=Math.exp(53);
+    let area=25*Math.PI;
+    console.log(number1);
+    console.log(number2);
+    console.log(number3);
+    console.log(min);
+    console.log(exp);
+    console.log(area);
+}
